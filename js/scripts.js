cca03e77da620c3057cc2b5a0aa1be77ac509f7f
Patrick Ryan <patrick.ryan@codeotter.com>
2019-01-26 01:26:18 -0800

    Tracking what song is responsible for what purchase

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
 // Empty JS for your own code to be here
+$(document).ready(function () {
+  Paddle.Setup({ vendor: 38954 });
+
+  var song = window.uQuery('song');
+
+  if (song) {
+    $('a.paddle_button').each(function () {
+      $(this).attr('data-passthrough', "{ \"song\": \"" + song + "\" }");
+    })
+  }
+});
